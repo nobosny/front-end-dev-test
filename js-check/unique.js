70e97ea8a5d7dba764d795afe0a9a0798761d770
Nobosny Perez <nobo.perez@uberops.com>
2018-03-10 15:04:11 -0500

    ⚡️ unique done

--- js-check/unique.js
+++ js-check/unique.js
@@ -13,8 +13,71 @@
 // handle nested objects:
 // [{foo: 'bar'}, {foo: 'bar'}] => [{foo: 'bar'}]
 
+// Since the arrays are flat and the order matters, we can use JSON strings to compare them.
+const compareArrays = (arr1, arr2) => {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
+// More complex object comparison and use of isEqual to check equality between values
+const compareObjects = (obj1, obj2) => {
+  const keysA = Object.getOwnPropertyNames(obj1);
+
+  if (keysA.length !== Object.getOwnPropertyNames(obj2).length) {
+    return false;
+  }
+
+  var equals = true;
+  keysA.forEach((key) => {
+    if ((typeof obj2[key] === 'undefined') || (!isEqual(obj1[key], obj2[key]))) {
+      equals = false;
+    }
+  });
+
+  return equals;
+}
+
+// Check for equality of two values
+const isEqual = (a, b) => {
+  const typeA = Object.prototype.toString.call(a);
+
+  // Check for equal types
+  if (typeA !== Object.prototype.toString.call(b)) {
+    return false;
+  }
+
+  switch (typeA) {
+    case '[object Array]':
+        // Compare arrays
+        return compareArrays(a, b);
+      break;
+    case '[object Object]':
+        // Compare Obj.keys && Obj.values arrays
+        return compareObjects(a, b);
+      break;
+    default:
+      // Base comparison
+      return a === b;
+  }
+
+  return true;
+}
+
+const findInArray = (item, arr) => {
+  return arr.findIndex((e) => {
+    return isEqual(e, item);
+  });
+}
+
 const unique = (arr) => {
-  // Add your code here.
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    if (findInArray(arr[0], arr.slice(1)) !== -1) {
+      return unique(arr.slice(1));
+    } else {
+      return [arr[0]].concat(unique(arr.slice(1)));
+    }
+  }
 }
 
 module.exports = unique
